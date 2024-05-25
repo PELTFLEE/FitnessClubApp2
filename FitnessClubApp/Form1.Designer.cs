135defada25956a0eccfd6524c0a9bc10df9842f
peltflee <peltflee@DESKTOP-NKURT15>
2024-05-25 13:45:12 +0300

    Добавьте файлы проекта.

---
+++ FitnessClubApp/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace FitnessClubApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WeightTextBox = new System.Windows.Forms.TextBox();
+            this.HeightTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ageBox = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // WeightTextBox
+            // 
+            this.WeightTextBox.Location = new System.Drawing.Point(112, 106);
+            this.WeightTextBox.Name = "WeightTextBox";
+            this.WeightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.WeightTextBox.TabIndex = 0;
+            // 
+            // HeightTextBox
+            // 
+            this.HeightTextBox.Location = new System.Drawing.Point(112, 132);
+            this.HeightTextBox.Name = "HeightTextBox";
+            this.HeightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.HeightTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Составить программу тренировок";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 9);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(417, 602);
+            this.listBox1.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.ageBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.HeightTextBox);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.WeightTextBox);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Location = new System.Drawing.Point(435, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(225, 238);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Введите информацию";
+            // 
+            // ageBox
+            // 
+            this.ageBox.Location = new System.Drawing.Point(112, 158);
+            this.ageBox.Name = "ageBox";
+            this.ageBox.Size = new System.Drawing.Size(100, 20);
+            this.ageBox.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(112, 80);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Фамилия клиента";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Имя клиента";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Цель занятий";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Рост клиента";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Вес клиента";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Возраст клиента";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(444, 588);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Сохранить программу тренировок";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 623);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox WeightTextBox;
+        private System.Windows.Forms.TextBox HeightTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox ageBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FitnessClubApp/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace FitnessClubApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double weight = Convert.ToDouble(WeightTextBox.Text);
+            double height = Convert.ToDouble(HeightTextBox.Text);
+            double age = Convert.ToDouble(ageBox.Text);
+
+
+            List<string> workouts = GenerateWorkoutProgramMan(weight, height, age);
+            List<string> workouts2 = GenerateWorkoutProgramWoman(weight, height, age);
+
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(workouts.ToArray());
+            listBox1.Items.AddRange(workouts2.ToArray());
+
+        }
+
+        private List<string> GenerateWorkoutProgramMan(double weight, double height, double age)
+        {
+            List<string> workouts = new List<string>();
+            //понедельник
+            int pullUp = (int)(weight * 0.03) * (int)(height * 0.05) * (int)(age * 0.08);
+            int tiltThrust = (int)(weight * 0.09) * (int)(height * 0.05) * (int)(age * 0.08);
+            int deadlift = (int)(weight * 0.09) * (int)(height * 0.05) * (int)(age * 0.08);
+            int mahi = (int)(weight * 0.08) * (int)(height * 0.05) * (int)(age * 0.08);
+            int bicepsStanding = (int)(weight * 0.09) * (int)(height * 0.05) * (int)(age * 0.08);
+            int hammerBends = (int)(weight * 0.08) * (int)(height * 0.05) * (int)(age * 0.08);
+            //понедельник
+
+            //среда
+            int benchPressWeight = (int)(weight * 0.09) * (int)(height * 0.05) * (int)(age * 0.08);
+            int legHorizontalBar = (int)(weight * 0.08) * (int)(height * 0.05) * (int)(age * 0.08);
+            int twistingLyingDown = (int)(weight * 0.05) * (int)(height * 0.05) * (int)(age * 0.08);
+            int benchPress = (int)(weight * 0.09) * (int)(height * 0.05) * (int)(age * 0.08);
+            //среда
+
+            //пятница
+            int FrencBenchPress = (int)(weight * 0.09) * (int)(height * 0.05) * (int)(age * 0.08);
+            int squatsWeight = (int)(weight * 0.1) * (int)(height * 0.05) * (int)(age * 0.08);
+            int legBends = (int)(weight * 0.1) * (int)(height * 0.05) * (int)(age * 0.08);
+            int pushupsOnTheUnevenBars = (int)(weight * 0.03) * (int)(height * 0.05) * (int)(age * 0.08);
+            int benchPressInTheBlock = (int)(weight * 0.09) * (int)(height * 0.05) * (int)(age * 0.08);
+
+            //пятница
+            workouts.Add("Программа тренировок для мужчин:");
+            //понедельник
+            workouts.Add("Понедельник (Спина/Бицепс) : ");
+            workouts.Add("1. Подтягивания с доп. весом: 4 подхода по 10 повторений " + pullUp + "кг ");
+            workouts.Add("2. Тяга штанги в наклоне: 4 подхода по 8 повторений " + tiltThrust + "кг ");
+            workouts.Add("3. Становая тяга: 3 подхода по 10 повторений " + deadlift + " кг");
+            workouts.Add("4. Махи гантелями в наклоне: 3 подхода по 15 повторений " + mahi + " кг");
+            workouts.Add("5. Подъем штанги на бицепс стоя: 4 подхода по 8 повторений " + bicepsStanding + " кг");
+            workouts.Add("6. Молотковые сгибания: 3 подхода по 8 повторений " + hammerBends + " кг");
+            //понедельник
+            workouts.Add("");
+            //среда
+            workouts.Add("Среда (Грудь/Пресс) : ");
+            workouts.Add("1. Жим лежа: 4 подхода по 8 повторений " + benchPressWeight + " кг");
+            workouts.Add("2. Подъем ног на турнике с доп. весом: 3 подхода по 12 повторений " + legHorizontalBar + "кг ");
+            workouts.Add("3. Скручивания лежа с доп. весом: 3 подхода по 15 повторений " + twistingLyingDown + " кг");
+            workouts.Add("4. Жим штанги сидя: 4 подхода по 15 повторений " + benchPress + " кг");
+            //среда
+            workouts.Add("");
+            //пятница
+            workouts.Add("Пятница (Трицепс/Ноги) : ");
+            workouts.Add("1. Французский жим лежа: 3 подхода по 10 повторений " + FrencBenchPress + " кг");
+            workouts.Add("2. Приседания со штангой: 4 подхода по 8 повторений " + squatsWeight + " кг");
+            workouts.Add("3. Разгибания ног: 3 подхода по 15 повторений " + legBends + " кг");
+            workouts.Add("4. Отжимания на брусьях с доп весом: 3 подхода по 10 повторений " + pushupsOnTheUnevenBars + " кг");
+            workouts.Add("5. Жим в блочном тренажере: 4 подхода по 8 повторений " + benchPressInTheBlock + " кг");
+            //пятница
+            workouts.Add("");
+            workouts.Add("");
+            workouts.Add("");
+            workouts.Add("");
+            return workouts;
+        }
+
+        private List<string> GenerateWorkoutProgramWoman(double weight, double height, double age)
+        {
+            List<string> workouts2 = new List<string>();
+            //понедельник
+            int pullUp = (int)(weight * 0.02) * (int)(height * 0.04) * (int)(age * 0.06);
+            int tiltThrust = (int)(weight * 0.08) * (int)(height * 0.04) * (int)(age * 0.06);
+            int deadlift = (int)(weight * 0.08) * (int)(height * 0.04) * (int)(age * 0.06);
+            int mahi = (int)(weight * 0.07) * (int)(height * 0.04) * (int)(age * 0.06);
+            int bicepsStanding = (int)(weight * 0.08) * (int)(height * 0.04) * (int)(age * 0.06);
+            int hammerBends = (int)(weight * 0.07) * (int)(height * 0.04) * (int)(age * 0.06);
+            //понедельник
+
+            //среда
+            int benchPressWeight = (int)(weight * 0.08) * (int)(height * 0.04) * (int)(age * 0.06);
+            int legHorizontalBar = (int)(weight * 0.07) * (int)(height * 0.04) * (int)(age * 0.06);
+            int twistingLyingDown = (int)(weight * 0.04) * (int)(height * 0.04) * (int)(age * 0.06);
+            int benchPress = (int)(weight * 0.08) * (int)(height * 0.04) * (int)(age * 0.06);
+            //среда
+
+            //пятница
+            int FrencBenchPress = (int)(weight * 0.08) * (int)(height * 0.04) * (int)(age * 0.06);
+            int squatsWeight = (int)(weight * 0.09) * (int)(height * 0.04) * (int)(age * 0.06);
+            int legBends = (int)(weight * 0.09) * (int)(height * 0.04) * (int)(age * 0.06);
+            int pushupsOnTheUnevenBars = (int)(weight * 0.02) * (int)(height * 0.04) * (int)(age * 0.06);
+            int benchPressInTheBlock = (int)(weight * 0.08) * (int)(height * 0.04) * (int)(age * 0.06);
+
+            //пятница
+            workouts2.Add("Программа тренировок для женщин:");
+            //понедельник
+            workouts2.Add("Понедельник (Спина/Бицепс) : ");
+            workouts2.Add("1. Подтягивания с доп. весом: 4 подхода по 10 повторений " + pullUp + "кг ");
+            workouts2.Add("2. Тяга штанги в наклоне: 4 подхода по 8 повторений " + tiltThrust + "кг ");
+            workouts2.Add("3. Становая тяга: 3 подхода по 10 повторений " + deadlift + " кг");
+            workouts2.Add("4. Махи гантелями в наклоне: 3 подхода по 15 повторений " + mahi + " кг");
+            workouts2.Add("5. Подъем штанги на бицепс стоя: 4 подхода по 8 повторений " + bicepsStanding + " кг");
+            workouts2.Add("6. Молотковые сгибания: 3 подхода по 8 повторений " + hammerBends + " кг");
+            //понедельник
+            workouts2.Add("");
+            //среда
+            workouts2.Add("Среда (Грудь/Пресс) : ");
+            workouts2.Add("1. Жим лежа: 4 подхода по 8 повторений " + benchPressWeight + " кг");
+            workouts2.Add("2. Подъем ног на турнике с доп. весом: 3 подхода по 12 повторений " + legHorizontalBar + "кг ");
+            workouts2.Add("3. Скручивания лежа с доп. весом: 3 подхода по 15 повторений " + twistingLyingDown + " кг");
+            workouts2.Add("4. Жим штанги сидя: 4 подхода по 15 повторений " + benchPress + " кг");
+            //среда
+            workouts2.Add("");
+            //пятница
+            workouts2.Add("Пятница (Трицепс/Ноги) : ");
+            workouts2.Add("1. Французский жим лежа: 3 подхода по 10 повторений " + FrencBenchPress + " кг");
+            workouts2.Add("2. Приседания со штангой: 4 подхода по 8 повторений " + squatsWeight + " кг");
+            workouts2.Add("3. Разгибания ног: 3 подхода по 15 повторений " + legBends + " кг");
+            workouts2.Add("4. Отжимания на брусьях с доп весом: 3 подхода по 10 повторений " + pushupsOnTheUnevenBars + " кг");
+            workouts2.Add("5. Жим в блочном тренажере: 4 подхода по 8 повторений " + benchPressInTheBlock + " кг");
+            //пятница
+            workouts2.Add("");
+            workouts2.Add("");
+            workouts2.Add("");
+            workouts2.Add("");
+            return workouts2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Текстовые файлы|*.txt";
+            saveFileDialog1.Title = "Сохранить программу тренировок";
+            saveFileDialog1.FileName = "Тренировка_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(saveFileDialog1.FileName))
+                {
+                    foreach (var item in listBox1.Items)
+                    {
+                        file.WriteLine(item.ToString());
+                    }
+                }
+
+                MessageBox.Show("Программа тренировок успешно сохранена.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
